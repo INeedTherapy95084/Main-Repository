34d5b84595398edd7658d997466936570a2d9db0
INeedTherapy95084 <shayan9afraz@gmail.com>
2024-05-28 18:48:57 +0600

    practise day 3

--- Python/practise/practise5.py
+++ Python/practise/practise5.py
@@ -1,6 +1,6 @@
 def palindrome(a):
     mid = (len(a)-1)//2
-    print(mid)  
+      
     start = 0 
     last = len(a)-1
     flag = 0
@@ -20,6 +20,34 @@ def palindrome(a):
     else:
         print("The entered string is not palindrome")
         
-name = "amkma"
+def symmetry(a):
+
+    n = len(a)
+    flag = 0
+
+    if n % 2:
+        mid = n//2 + 1
+    else:
+        mid = n//2
+
+    start1 = 0
+    start2 = mid
+
+    while(start1 < mid and start2 < n):
+
+        if (a[start1] == a[start2]):
+            start1 = start1 + 1
+            start2 = start2 + 1
+        else:
+            flag = 1
+            break
+        
+    if flag == 0:
+        print("The entered string is symmetrical")
+    else:
+        print("The entered string is not symmetrical")
+        
+name = input("Type a name: ")
 
 palindrome(name)
+symmetry(name)
