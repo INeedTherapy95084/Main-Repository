d5a38b73f1dd55467befd644552a88ac8c9e2867
INeedTherapy95084 <shayan9afraz@gmail.com>
2024-05-30 18:46:21 +0600

    practise day 5

---
+++ Python/practise/practise9.py
@@ -0,0 +1,8 @@
+import sys
+tuple1 = ("A", 1, "B", 2, "C", 3)
+tuple2 = ("Ajnhbgvfcdsx", "Bnhbgvfcdsdcfvg", "Cmjnhbgvfcdxsz")
+tuple3 = (1, 2, 3, 4, 5, 6)
+
+print("Size of Tuple1: " + str(sys.getsizeof(tuple1)) + "bytes")
+print("Size of Tuple2: " + str(sys.getsizeof(tuple2)) + "bytes")
+print("Size of Tuple3: " + str(sys.getsizeof(tuple3)) + "bytes")
