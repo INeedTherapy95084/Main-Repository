80a85711d2734f8d3b5d3b7a1f5f5e4e02e1c26d
INeedTherapy95084 <shayan9afraz@gmail.com>
2024-05-31 16:13:17 +0600

    practice day 6

---
+++ Python/practise/practice14.py
@@ -0,0 +1,9 @@
+import sys
+
+dic1 = {"A": 1, "B": 2, "C": 3}
+dic2 = {"Geek1": "Raju", "Geek2": "Nikhil", "Geek3": "Deepanshu"}
+dic3 = {1: "Lion", 2: "Tiger", 3: "Fox", 4: "Wolf"}
+
+print("Size of dic1: " + str(sys.getsizeof(dic1)) + "bytes")
+print("Size of dic2: " + str(sys.getsizeof(dic2)) + "bytes")
+print("Size of dic3: " + str(sys.getsizeof(dic3)) + "bytes")
