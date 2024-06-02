ec66839b3dc472a042f6b331b5f57972707a335b
INeedTherapy95084 <shayan9afraz@gmail.com>
2024-06-02 22:13:29 +0600

    practice day 8

---
+++ Python/practise/practice18.py
@@ -0,0 +1,13 @@
+a = [1, 2, 3, 4, 5]
+b=[6, 7, 8, 9]
+
+for num in a:
+    for num2 in b:
+        if num == num2:
+            print("True")
+            quit()
+        elif num != a[-1]:
+            continue
+        else:
+            print("False")
+            quit()
