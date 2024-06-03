8a02efb92526a931cbd4e1f5eb7577db7836eed3
INeedTherapy95084 <shayan9afraz@gmail.com>
2024-06-03 14:33:50 +0600

    failed

---
+++ Python/practise/practice19.py
@@ -0,0 +1,13 @@
+test_list = [[5, 8, 10],
+             [2, 0, 9],
+             [5, 4, 2],
+             [2, 3, 9]]
+i = 1
+result_dct = {}
+
+for num in test_list[0]:
+    for lst in test_list:
+        result_dct = ()
+        i += 1
+
+print(result_dct)
