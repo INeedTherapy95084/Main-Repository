065bf0b5106b117d4914912ee35dd0c7e1db09c1
INeedTherapy95084 <shayan9afraz@gmail.com>
2024-06-23 10:23:54 +0600

    help-

--- Python/Rock_Paper_Sissors_Game.py
+++ Python/Rock_Paper_Sissors_Game.py
@@ -8,13 +8,21 @@ def showChoice(x):
     
     return choices.get(x)
 
+score = 0
+
 def check_winner(player, computer):
     if player == 1 and computer == 3:
         print('You Win!\n')
+        win = True
+        return win
     elif player == 2 and computer == 1:
         print('You Win!\n')
+        win = True
+        return win
     elif player == 3 and computer == 2:
         print('You Win!\n')
+        win = True
+        return win
     elif player == computer:
         print('Tie!\n')
     else:
@@ -24,19 +32,20 @@ while True:
     player = 0
 
     while player > 0 or player <= 3 or player == "exit" or  player == "Exit":
-        playerChoice = input("Enter your choice:\n 1 for Rock\n 2 for Paper\n 3 for Sissors\n\n")
+        playerChoice = input("Enter Your Choice:\n 1 For Rock\n 2 For Paper\n 3 For Sissors\n\n")
         try:
             if player == "exit" or  player == "Exit":
                 quit()
             player = int(playerChoice)
             break
         except:
-            print("INVALID_CHCOICE_ENTERED")
+            print("\nINVALID_CHCOICE_ENTERED\n")
             
 
     computer = int(random.choice("123"))
 
-    print(f"\nPlayer choice: {showChoice(player)}")
-    print(f"Computer choice: {showChoice(computer)} \n")
+    print(f"\nPlayer Choice: {showChoice(player)}")
+    print(f"Computer Choice: {showChoice(computer)} \n")
 
     check_winner(player, computer)
+    print(f"Your Score: {score}\n")
