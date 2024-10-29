73f17b093a265760d79bf2df9cf559b924d95f87
INeedTherapy95084 <shayan9afraz@gmail.com>
2024-10-29 11:16:31 +0600

    .

---
+++ Python/AutoClicker.py
@@ -0,0 +1,21 @@
+import os
+import time
+import pyautogui as gui
+import keyboard
+
+os.system('cls')
+
+print("Autoclicker is running...")
+
+stop_region = (0, 1031, 1900, 50)
+
+while True:
+    if keyboard.is_pressed('p'):
+        while True:
+            gui.leftClick()
+            if keyboard.is_pressed('l'):
+                break
+            time.sleep(0.1)
+    if keyboard.is_pressed('`'):
+        break
+os.system('cls')
--- Python/Tester3.py
+++ Python/Tester3.py
@@ -2,7 +2,7 @@ import pyautogui as gui
 import time
 
 
-ss = gui.screenshot(region=(10, 300, 300, 540))
+ss = gui.screenshot(region=(0, 1031, 1900, 50))
 ss.save(r"D:\E Drive\vscode\Private-Repository\output_image.jpg")
 
 
